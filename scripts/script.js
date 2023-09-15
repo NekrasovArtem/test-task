5e1d2ba3c0fd82aead7514e11e10a2f678ba74be
NekrasovArtem <art3midus@yandex.com>
2023-09-15 21:18:26 +0500

    Добавлены иконки соц сетей

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,31 @@
+//Открытие и закрытие меню социальных сетей
+let social = document.querySelector('#social');
+let icons = document.querySelector('#icons');
+let leftArrow = document.querySelector('#leftArrow');
+let rightArrow = document.querySelector('#rightArrow');
+let socialBool = false;
+social.addEventListener('click', () => {
+    if (socialBool === false) {
+        socialBool = true;
+        rightArrow.style.display = "none";
+        leftArrow.style.display = "block";
+        icons.classList.add("showIcons");
+    } else {
+        socialBool = false;
+        leftArrow.style.display = "none";
+        rightArrow.style.display = "block";
+        icons.classList.remove("showIcons");
+    }
+})
+
+//Смена фотографи
 let image = document.querySelector('#img');
 image.addEventListener('click', () => {
     image.src = "images/foto_2.jpg";
 })
+
+
+//Показ и скрытие подробностей об авторе
 let section = document.querySelector('.info');
 let body = document.querySelector('body');
 let btnMore = document.querySelector('#btnMore');
